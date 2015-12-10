53202fe34b3a93ab912f4863aba168050a7a5a64
maheshadike <mahesh.adike@hotmail.com>
2015-12-09 21:06:13 -0800

    change done

--- Tables/HumanResources.Shift.sql
+++ Tables/HumanResources.Shift.sql
@@ -19,8 +19,10 @@ EXEC sp_addextendedproperty N'MS_Description', N'Shift end time.', 'SCHEMA', N'H
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Date and time the record was last updated.', 'SCHEMA', N'HumanResources', 'TABLE', N'Shift', 'COLUMN', N'ModifiedDate'
 GO
-EXEC sp_addextendedproperty N'MS_Description', N'Shift description.', 'SCHEMA', N'HumanResources', 'TABLE', N'Shift', 'COLUMN', N'Name'
+
+EXEC sp_addextendedproperty N'MS_Description', N'ITmahesh', 'SCHEMA', N'HumanResources', 'TABLE', N'Shift', 'COLUMN', N'Name'
 GO
+
 EXEC sp_addextendedproperty N'MS_Description', N'Primary key for Shift records.', 'SCHEMA', N'HumanResources', 'TABLE', N'Shift', 'COLUMN', N'ShiftID'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Shift start time.', 'SCHEMA', N'HumanResources', 'TABLE', N'Shift', 'COLUMN', N'StartTime'
